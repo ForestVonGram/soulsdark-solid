d5a178bde6a54278ad8a1f1266819e3441905257
Forest <playa200612@gmail.com>
2024-02-21 12:38:44 -0500

    chi uwu

---
+++ src/main/java/org/example/Juego.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Juego {
+    private Collection<Jugador> jugadores;
+    private Collection<Personaje> personajes;
+    public Juego() {
+        this.jugadores = new LinkedList<>();
+        this.personajes = new LinkedList<>();
+    }
+
+    public Collection<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void crearJugador(String id) {
+        assert id != null && !id.isBlank();
+        Jugador jugador = new Jugador(id);
+
+        jugadores.add(jugador);
+    }
+
+    public void registrarPersonaje(Personaje personaje) {
+        assert personaje != null;
+        personajes.add(personaje);
+    }
+}
---
+++ src/main/java/org/example/Jugador.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Jugador implements Personalizacion{
+    private final String id;
+
+    public Jugador(String id) {
+
+        this.id = id;
+    }
+}
